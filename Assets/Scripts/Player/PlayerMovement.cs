8cb9a14a7f9daed5afc2c7b8037b39de264be749
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-08-15 00:53:21 -0400

    Add Jumping and WASD Movement to playermovement script

--- Assets/Scripts/Player/PlayerMovement.cs
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -5,58 +5,72 @@ using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
-    [SerializeField] private float speed = 5f;
+    [SerializeField] private float moveSpeed = 5f;
     [SerializeField] private float jumpForce = 10f;
 
     private PlayerInput playerInput;
+    // private InputAction moveAction;
+    private InputAction jumpAction;
+
     private Rigidbody rb;
-    private Collider col;
-    private Vector3 movementInput;
     private bool isGrounded;
+    private Vector2 moveDirection;
 
 
     private void Awake()
     {
         playerInput = GetComponent<PlayerInput>();
+        // moveAction = playerInput.actions["Move"];
+        jumpAction = playerInput.actions["Jump"];
+
         rb = GetComponent<Rigidbody>();
     }
 
 
     private void OnEnable()
     {
-        playerInput.actions["Move"].started += OnMovementStarted;
-        playerInput.actions["Move"].canceled += OnMovementCanceled;
-        playerInput.actions["Jump"].performed += ctx => Jump();
+        jumpAction.performed += ctx => Jump();
     }
     private void OnDisable()
     {
-        playerInput.actions["Move"].started -= OnMovementStarted;
-        playerInput.actions["Move"].canceled -= OnMovementCanceled;
-        playerInput.actions["Jump"].performed -= ctx => Jump();
+        jumpAction.performed -= ctx => Jump();
     }
 
 
-    private void OnMovementStarted(InputAction.CallbackContext context)
-    {
-        movementInput = context.ReadValue<Vector3>();
+    // Move InputAction
+    private void OnMove(InputValue value) {
+        moveDirection = value.Get<Vector2>();
     }
-    private void OnMovementCanceled(InputAction.CallbackContext context)
+
+
+    private void OnCollisionEnter(Collision collision)
     {
-        movementInput = Vector3.zero;
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = true;
+        }
+    }
+    private void OnCollisionExit(Collision collision)
+    {
+        if (collision.gameObject.layer == LayerMask.NameToLayer("Ground"))
+        {
+            isGrounded = false;
+        }
     }
 
 
     private void FixedUpdate()
     {
         Move();
-
     }
 
 
     private void Move()
     {
-        Vector3 movement = movementInput * speed;
-        rb.velocity = new Vector3(movement.x, rb.velocity.y);
+        Vector3 velocity = new (moveDirection.x, 0f, moveDirection.y);
+        velocity *= moveSpeed;
+        velocity.y = rb.velocity.y;
+        rb.velocity = velocity;
     }
 
 
